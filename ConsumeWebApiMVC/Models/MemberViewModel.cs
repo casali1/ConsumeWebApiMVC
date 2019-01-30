c1cb1c6d2e65a7393ec4288440d6248d0947d312
casali1 <frank_casali@hotmail.com>
2019-01-29 21:54:27 -0700

    Recording of ConsumeWebApiMVC.

---
+++ ConsumeWebApiMVC/Models/MemberViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConsumeWebApiMVC.Models
+{
+    public class MemberViewModel
+    {
+        public int MemberID { get; set; }
+        public string MemberName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
