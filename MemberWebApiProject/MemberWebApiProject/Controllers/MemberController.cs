72ed3388d5307e82842647dddcda514d69bd99e3
casali1 <frank_casali@hotmail.com>
2019-05-28 23:02:56 -0600

    Create CRUD Web API

---
+++ MemberWebApiProject/MemberWebApiProject/Controllers/MemberController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MemberWebApiProject.Models;
+using MemberWebApiProject.Service;
+
+namespace MemberWebApiProject.Controllers
+{
+    public class MemberController : ApiController
+    {
+        ProcessMembers data = new ProcessMembers();
+
+        public IEnumerable<Member> Get()
+        {
+            return data.GetMembers();
+        }
+
+        public Member Get(int id)
+        {
+            return data.GetMembers().FirstOrDefault(x => x.MemberID == id);
+        }
+    }
+}
